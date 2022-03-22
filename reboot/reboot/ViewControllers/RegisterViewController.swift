349ac5b3bf4f610a8e7df1fa6ac434787541f00f
EungJin Kim <kwilliam777@gmail.com>
2022-03-22 15:10:31 -0500

    QR

--- reboot/reboot/ViewControllers/RegisterViewController.swift
+++ reboot/reboot/ViewControllers/RegisterViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
+    @IBOutlet weak var btnForLoginViewController: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -15,4 +16,12 @@ class RegisterViewController: UIViewController {
         self.navigationController?.isNavigationBarHidden = true
         
     }
+    
+    @IBAction func onLoginViewControllerBtnClicked(_ sender: UIButton) {
+        print("RegisterViewController - onLoginViewControllerBtnClicked() called")
+        
+        //네비게이션 뷰 컨트롤러를 팝한다
+        self.navigationController?.popViewController(animated: true)
+    }
+    
 }
