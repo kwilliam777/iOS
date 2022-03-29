0517f21379ba1f3b15beab2c08d4744a61bba8e0
EungJin Kim <kwilliam777@gmail.com>
2022-03-29 18:51:57 -0500

    navigation bar with storyboard

---
+++ navbar_with_storyboard/navbar_with_storyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  navbar_with_storyboard
+//
+//  Created by 김응진 on 2022/03/29.
+//
+
+import UIKit
+
+class ViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
