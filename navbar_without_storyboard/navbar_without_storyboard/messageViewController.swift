7ac875a38059425884ea987ef7395bc689fee718
EungJin Kim <kwilliam777@gmail.com>
2022-03-29 19:13:01 -0500

    navbar without storyboard

---
+++ navbar_without_storyboard/navbar_without_storyboard/messageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  messageViewController.swift
+//  navbar_without_storyboard
+//
+//  Created by 김응진 on 2022/03/29.
+//
+
+import UIKit
+
+class messageViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "메세지"
+        self.view.backgroundColor = .systemMint
+        
+    }
+}
---
+++ navbar_without_storyboard/navbar_without_storyboard/profileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  profileViewController.swift
+//  navbar_without_storyboard
+//
+//  Created by 김응진 on 2022/03/29.
+//
+
+import UIKit
+
+class profileViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "프로필"
+        self.view.backgroundColor = .purple
+        
+        
+    }
+}
