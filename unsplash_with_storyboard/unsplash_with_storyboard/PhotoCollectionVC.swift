16bbb7a20dabd32f5e153155e77fa262474c97f0
EungJin Kim <kwilliam777@gmail.com>
2022-04-04 20:22:47 -0500

    unsplash -ing

---
+++ unsplash_with_storyboard/unsplash_with_storyboard/PhotoCollectionVC.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoCollectionVC.swift
+//  unsplash_with_storyboard
+//
+//  Created by 김응진 on 2022/04/04.
+//
+
+import UIKit
+
+class PhotoCollectionVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("PhotoCollectionVC - viewDidLoad")
+        
+    }
+}
---
+++ unsplash_with_storyboard/unsplash_with_storyboard/UserListVC.swift
@@ -0,0 +1,16 @@
+//
+//  UserListVC.swift
+//  unsplash_with_storyboard
+//
+//  Created by 김응진 on 2022/04/04.
+//
+
+import UIKit
+
+class UserListVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("UserListVC - viewDidLoad")
+    }
+}
