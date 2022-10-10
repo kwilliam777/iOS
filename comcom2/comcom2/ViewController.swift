0675e62f76182449a11aad0253596b4ba72c9598
EungJin Kim <kwilliam777@gmail.com>
2022-10-11 00:00:51 +0900

    comcom2 setting

--- comcom2/comcom2/ViewController.swift
+++ comcom2/comcom2/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var email: UITextField!
+    @IBOutlet var isUpdate: UISwitch!
+    @IBOutlet var interval: UIStepper!
+    @IBOutlet var isUpdateTxt: UILabel!
+    @IBOutlet var intervalTxt: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func onSwitch(_ sender: UISwitch) {
+    }
+    @IBAction func onStepper(_ sender: UIStepper) {
+    }
+    
 }
 
