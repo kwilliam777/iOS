bd6c769449afd6224685e0c46e9e5588c8b418a7
EungJin Kim <kwilliam777@gmail.com>
2022-10-11 11:49:49 +0900

    send data with navigation

--- comcom2/comcom2/ViewController.swift
+++ comcom2/comcom2/ViewController.swift
@@ -31,8 +31,11 @@ class ViewController: UIViewController {
         rvc.paramEmail = self.email.text!
         rvc.paramUpdate = self.isUpdate.isOn
         rvc.paramInterval = self.interval.value
+//        버튼으로 이동할 경우
+//        self.present(rvc,animated: true)
         
-        self.present(rvc,animated: true)
+//        네비게이션바로 이동할 경우
+        self.navigationController?.pushViewController(rvc, animated: true)
     }
     
 }
