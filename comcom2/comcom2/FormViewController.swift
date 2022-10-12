c7fd714e370b3c5f77e40dc5751e90b34fad7796
EungJin Kim <kwilliam777@gmail.com>
2022-10-12 11:27:09 +0900

    send data backward setting

---
+++ comcom2/comcom2/FormViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FormViewController.swift
+//  comcom2
+//
+//  Created by 김응진 on 2022/10/12.
+//
+
+import UIKit
+
+class FormViewController: UIViewController {
+    
+    
+    
+    
+    
+}
--- comcom2/comcom2/ResultViewController.swift
+++ comcom2/comcom2/ResultViewController.swift
@@ -9,20 +9,37 @@ import UIKit
 
 class ResultViewController: UIViewController {
     @IBOutlet var resultEmail: UILabel!
-//    @IBOutlet var resultUpdate: UILabel!
     @IBOutlet var resultUpdate: UILabel!
-    
     @IBOutlet var resultInterval: UILabel!
     
     var paramEmail: String = ""
     var paramUpdate: Bool = false
     var paramInterval: Double = 0
     
+    @IBOutlet var resultEmail2: UILabel!
+    @IBOutlet var resultUpdate2: UILabel!
+    @IBOutlet var resultInterval2: UILabel!
+    
+    var paramEmail2: String?
+    var paramUpdate2: Bool?
+    var paramInterval2: Double?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.resultEmail.text = paramEmail
         self.resultUpdate.text = (self.paramUpdate == true ? "자옫갱신" : "자동갱신안함")
         self.resultInterval.text = "\(Int(paramInterval))분 마다 갱신"
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        if let email = paramEmail2 {
+            resultEmail2.text = email
+        }
+        if let update = paramUpdate2 {
+            resultUpdate2.text = update == true ? "자동갱신":"자동갱신안합"
+        }
+        if let interval = paramInterval2 {
+            resultInterval2.text = "\(Int(interval))분마다"
+        }
     }
     
     
