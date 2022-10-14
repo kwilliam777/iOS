45a6886a7b07ec27356f5de1de6e39eb3a9dd146
EungJin Kim <kwilliam777@gmail.com>
2022-10-14 11:46:15 +0900

    alert text field

--- comcom2/comcom2/FormViewController.swift
+++ comcom2/comcom2/FormViewController.swift
@@ -43,15 +43,45 @@ class FormViewController: UIViewController {
     }
     
     @IBAction func alert(_ sender: Any) {
-        let alert = UIAlertController(title: "선택", message: "항목을 선택해주세요", preferredStyle: .alert)
+        let alert = UIAlertController(title: "선택", message: "항목을 선택해주세요", preferredStyle: .actionSheet)
         
-        let cancel = UIAlertAction(title: "취소", style: .cancel)
+        let cancel = UIAlertAction(title: "취소", style: .cancel) { (_) in self.result.text = "취소버튼을 클릭!"}
+        let ok = UIAlertAction(title: "확인", style: .default) { (_) in self.result.text = "확인버튼을 클릭!"}
+        let exec = UIAlertAction(title: "실행", style: .destructive) { (_) in self.result.text = "실행버튼을 클릭!"}
+        let stop = UIAlertAction(title: "중지", style: .default) { (_) in self.result.text = "중지버튼을 클릭!"}
         
+        alert.addAction(ok)
         alert.addAction(cancel)
+        alert.addAction(exec)
+        alert.addAction(stop)
         
         self.present(alert, animated: true)
     }
     
+    @IBAction func login(_ sender: Any) {
+        let title = "iTunes Stroe에 로그인"
+        let message = "사용자의 Apple ID sqlpro@naver.com의 암호를 입력하십시오"
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let cancel = UIAlertAction(title: "취소", style: .cancel)
+        let ok = UIAlertAction(title: "확인", style: .default) { (_) in
+            if let tf = alert.textFields?[0] {
+                print("입력된 값은 \(tf.text!) 입니다.")
+            } else {
+                print("입력된 값이 없습니다.")
+            }
+        }
+        alert.addAction(cancel)
+        alert.addAction(ok)
+        
+        alert.addTextField(configurationHandler: { (tf) in
+            tf.placeholder = "암호"
+            tf.isSecureTextEntry = true
+        })
+        
+        self.present(alert,animated: true)
+        
+    }
     
     
 }
