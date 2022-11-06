821eb806c49d8c7ba355c642938ec41a0960323d
EungJin Kim <kwilliam777@gmail.com>
2022-11-06 13:29:20 +0900

    text field delegate

--- comcom3/comcom3/ViewController.swift
+++ comcom3/comcom3/ViewController.swift
@@ -12,10 +12,29 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tf.placeholder = "please enter text"
+        self.tf.keyboardType = UIKeyboardType.alphabet
+        self.tf.keyboardAppearance = UIKeyboardAppearance.dark
+        self.tf.returnKeyType = UIReturnKeyType.default
+        self.tf.enablesReturnKeyAutomatically = true
         
+        self.tf.borderStyle = UITextField.BorderStyle.line
+        self.tf.backgroundColor = UIColor(white:0.87, alpha: 1.0)
+        self.tf .contentVerticalAlignment = .center
+        self.tf.contentHorizontalAlignment = .center
+        self.tf.layer.borderColor = UIColor.darkGray.cgColor
+        self.tf.layer.borderWidth = 2.0
         
+        self.tf.becomeFirstResponder()
     }
 
-
+    @IBAction func confirm(_ sender: Any) {
+        self.tf.resignFirstResponder()
+    }
+    
+    @IBAction func input(_ sender: Any) {
+        self.tf.becomeFirstResponder()
+    }
+    
 }
 
