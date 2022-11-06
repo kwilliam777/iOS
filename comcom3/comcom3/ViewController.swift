7bf05ea0bb9d23a6beb728dac7fa91100eacd1b7
EungJin Kim <kwilliam777@gmail.com>
2022-11-06 13:16:27 +0900

    start delegate

---
+++ comcom3/comcom3/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  comcom3
+//
+//  Created by 김응진 on 2022/11/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var tf: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
